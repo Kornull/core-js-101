6ffc117c422d9275794fc37a96afb01e33bed70e
Kornull <wolod1987kor@gmail.com>
2022-07-22 00:32:52 +0300

    feat :add

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -56,7 +56,7 @@ function parseDataFromIso8601(value) {
 function isLeapYear(date) {
   const year = date.getFullYear();
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) return true;
-  return false
+  return false;
 }
 
 
